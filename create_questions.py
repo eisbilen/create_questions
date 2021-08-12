449baa70d69b45372a042666ec053b512b70fa58
eisbilen <erdemisbilen@gmail>
2021-08-13 00:00:42 +0100

    commit on main

--- create_questions.py
+++ create_questions.py
@@ -1,11 +1,29 @@
 import json
 import spacy
+from spacy.matcher import Matcher
+
 import sqlite3
 
 nlp = spacy.load('en_core_web_sm')
+matcher_prepositions_sub_conj = Matcher(nlp.vocab)
+prepositions_sub_conj_rule_1 = [[{'TAG': 'IN'}]]
+matcher_prepositions_sub_conj.add('PREPOSITION', prepositions_sub_conj_rule_1)
 
 print(nlp)
 print('success')
+
+def write_linguistic_features(matches_single, my_doc):
+    lf_dict = {}
+    lf_list = []
+    for match_id, start, end in matches_single:
+        lf_dict = {}
+        # Get the string representation
+        string_id = nlp.vocab.strings[match_id]
+        span = my_doc[start:end]  # The matched span
+        lf_dict[string_id] = span.text
+        lf_list.append(lf_dict)
+        print(match_id, string_id, start, end, span.text)
+    return lf_list
 
 def get_all():
     try:
@@ -20,7 +38,12 @@ def get_all():
         records = cursor.fetchall()
 
         for row in records:
-            print(dict(row))
+            sentence = dict(row)['sentence']
+
+            my_doc = nlp(sentence)
+            matches_prepositions_sub_conj = matcher_prepositions_sub_conj(my_doc)
+            preposition = write_linguistic_features(matches_prepositions_sub_conj, my_doc)
+            print(preposition)
         cursor.close()
 
     except sqlite3.Error as error:
